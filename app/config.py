4c906d0f1101ddd1a193e27a7ff675e30a99c209
carl-castell <118299961+carl-castell@users.noreply.github.com>
2023-04-14 10:47:51 +0200

    form

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
 SECRET_KEY = environ.get('SECRET_KEY')
+TOPICS_PER_PAGE = 4
--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,10 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app, request
 from .models import Topic
 from flask_login import login_required
 
 blueprint = Blueprint('posts', __name__)
 
 @blueprint.get('/topics')
-@login_required
+#@login_required
 def topics():
-    return render_template('posts/overview.html')
+    return render_template('topics/index.html')
+
+
+@blueprint.get('/topics/show')
+#@login_required
+def show_topic(id):
+    return render_template('topics/index.html')
+
+@blueprint.get('/topics/new')
+#@login_required
+def new_topic():
+    return render_template('posts/new_topic.html')
