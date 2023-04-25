46db41cdd28f5737941e5b270735e44ba0981854
carl-castell <118299961+carl-castell@users.noreply.github.com>
2023-04-25 22:09:52 +0200

    clean up mess

--- app/simple_pages/routes.py
+++ app/simple_pages/routes.py
@@ -1,17 +1,9 @@
 
 from flask import Blueprint, render_template, redirect, url_for, send_file
 from app.users.models import User
-from os import environ
-
-secret= "/",environ['DATABASE_URL']
 
 blueprint = Blueprint('simple_pages', __name__)
 
 @blueprint.route('/')
 def index():
   return render_template('index.html')
-
-#for google verification
-@blueprint.route(secret)
-def google_site_verf():
-    return url_for('/etc/secrets/google')
